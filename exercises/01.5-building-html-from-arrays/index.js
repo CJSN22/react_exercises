2ec09206be4d20d41b41fcbf63890bf11462913b
Charles Fabricius <cjsn22@gmail.com>
2019-06-25 03:13:15 +0000

    first commit

---
+++ exercises/01.5-building-html-from-arrays/index.js
@@ -0,0 +1,27 @@
+import React from "react"; //Main React.js library
+import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
+
+// only update the value of this array
+const navlinkItems = [
+	<ul key="nav" className="nav">
+		<li key="nav" className="nav-item">
+			<a className="nav-link" href="#">
+				Link to google.com
+			</a>
+		</li>
+		<li key="nav" className="nav-item">
+			<a className="nav-link" href="#">
+				Link to facebook.com
+			</a>
+		</li>
+		<li key="nav" className="nav-item">
+			<a className="nav-link" href="#">
+				Link to amazon.com
+			</a>
+		</li>
+	</ul>
+];
+
+const content = <ul className="nav">{navlinkItems}</ul>;
+
+ReactDOM.render(content, document.querySelector("#myDiv"));
